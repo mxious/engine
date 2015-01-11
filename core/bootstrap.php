2165f1e9e65baddd90d31cb820fe9a7943f7b30a
Sergio <sergiomattei@outlook.com>
2015-01-11 18:11:10 -0400

    Added Idiorm ORM and enhanced configuration file

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -11,21 +11,45 @@
  * 
  */
 
-# Require necessary classes
-require("config/constants.php");
-require("config/database.php");
+try {
+	# Require necessary classes.
+	if (!@require("config/constants.php")) {
+		throw new Exception("Constants file not found.");
+	}
+	if (!@require("../vendor/libraries/idiorm.php")) {
+		throw new Exception("Idiorm ORM not found.");	
+	}
 
-switch (Mxious_Defines::SITE_MODE) {
-	case 'development':
-		error_reporting(E_ALL);
-		break;
-	
-	case 'production':
-		error_reporting(0);
-		break;
-
-	default:
-		throw new Exception("Site mode is not set. Falling back to development.");
-		break;
+	switch (Constants::SITE_MODE) {
+		case 'development':
+			error_reporting(E_ALL);
+			break;
 		
+		case 'production':
+			error_reporting(0);
+			break;
+
+		default:
+			throw new Exception("Site mode is not set.");
+			break;	
+	}
+} catch(Exception $e) {
+	echo $e->getMessage();
+	die("Could not start the MxiousEngine.");
 }
+
+# Start Idiorm ORM Configuration
+ORM::configure('mysql:host='.Constants::DATABASE['dbname'].';dbname='.Constants::DATABASE['dbname']);
+ORM::configure('username', Constants::DATABASE['username']);
+ORM::configure('password', Constants::DATABASE['password']);
+
+/*
+
+	Everything has been loaded.
+	No thermonuclear war has happened, hopefully.
+	Do good, not evil.
+
+	$cats_saved = 45;
+
+ */
+
--- core/configs/constants.php
+++ core/configs/constants.php
@@ -11,22 +11,34 @@
  * 
  */
 
-class Mxious_Defines {
+class Constants {
 
 	// The site's url. Start this with http:// prefix and trailing slash.
-	static const BASE_URL = "http://example.com/";
+	const BASE_URL = "http://example.com/";
 
 	// This program has CDN support. If you use a CDN, place the URL to the CDN here, and USE_CDN to true.
-	static const USE_CDN = false;
-	static const CDN_URL = 'https://cdn.example.com/';
+	const USE_CDN = false;
+	const CDN_URL = 'https://cdn.example.com/';
 
 	// Site mode. Pretty self explanatory. If site mode is production, errors will be silenced.
-	static const SITE_MODE = 'development';
+	// Types:
+	// closed = dies
+	// production = silences errors
+	// development = displays errors
+	
+	const SITE_MODE = 'development';
 
 	// Place any other constants you need throughout the application after this line.
 	// Example:
-	// static const FACEBOOK_APIKEY = "apikey1234";
-	// static const TWITTER_APIKEY = "apikey1234";
+	// const FACEBOOK_APIKEY = "apikey1234";
+	// const TWITTER_APIKEY = "apikey1234";
 
+	// Database constant. Fill this out to use the Idiorm ORM (required)
+	const DATABASE = [
+		'db_name' => 'mxious',
+		'username' => 'root',
+		'password' => '',
+		'host' => 'localhost'
+	];
 }
 
