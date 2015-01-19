9e942a11e88c07969f503bcf8af9ac2f8ae8621c
Sergio <sergiomattei@outlook.com>
2015-01-18 23:08:24 -0400

    Fixed namespace issue with the Signature authentication library, code works fully now

--- vendor/Signature/includes.php
+++ vendor/Signature/includes.php
@@ -4,11 +4,11 @@
 include("Auth.php");
 include("Request.php");
 include("Token.php");
+include("Guards/Guard.php");
 include("Guards/CheckVersion.php");
 include("Guards/CheckTimestamp.php");
 include("Guards/CheckSignature.php");
 include("Guards/CheckKey.php");
-include("Guards/Guard.php");
 include("Exceptions/SignatureException.php");
 include("Exceptions/SignatureKeyException.php");
 include("Exceptions/SignatureSignatureException.php");
