b1f11c016a558e6bc3e835811ec92be7c54bad12
Sergio <sergiomattei@outlook.com>
2015-01-19 18:32:55 -0400

    Added PHPUnit cases

--- tests/SlimTest.php
+++ tests/SlimTest.php
@@ -15,9 +15,27 @@ class MxiousTest extends PHPUnit_Framework_TestCase {
         ));
     }
 
-    public function test_test() {
+    public function testphpunit() {
         $int = 5;
         $this->assertEquals($int, 5);
+    }
+
+   public function request($method, $path, $options = array()) {
+        ob_start();
+        $app = new \Slim\Slim();
+        $this->app = $app;
+        $this->request = $app->request();
+        $this->response = $app->response();
+        return ob_get_clean();
+    }
+
+    public function get($path, $options = array()) {
+        $this->request('GET', $path, $options);
+    }
+
+    public function testIndex() {
+        $this->get('/');
+        $this->assertEquals('200', $this->response->status());
     }
 
 }
