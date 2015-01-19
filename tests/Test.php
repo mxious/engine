2967a24512d44731a7ed3df5adb02ce615e65664
Sergio <sergiomattei@outlook.com>
2015-01-19 17:05:14 -0400

    Added test phpunit case

---
+++ tests/Test.php
@@ -0,0 +1,43 @@
+<?php
+require_once __DIR__ . '/../vendor/Slim/vendor/autoload.php';
+
+use Slim\Environment;
+
+// Example of Unit test taken from http://blog.shahariaazam.com/write-unit-test-for-php-slim-framework/#.VL1wYc3d_Dc.
+// Thanks man!
+
+class RoutesTest extends PHPUnit_Framework_TestCase
+{
+
+    public function request($method, $path, $options = array())
+    {
+        // Capture STDOUT
+        ob_start();
+
+        // Prepare a mock environment
+        Environment::mock(array_merge(array(
+            'REQUEST_METHOD' => $method,
+            'PATH_INFO' => $path,
+            'SERVER_NAME' => 'slim-test.dev',
+        ), $options));
+
+        $app = new \Slim\Slim();
+        $this->app = $app;
+        $this->request = $app->request();
+        $this->response = $app->response();
+
+        // Return STDOUT
+        return ob_get_clean();
+    }
+
+    public function get($path, $options = array())
+    {
+        $this->request('GET', $path, $options);
+    }
+
+    public function testIndex()
+    {
+        $this->get('/');
+        $this->assertEquals('200', $this->response->status());
+    }
+}
